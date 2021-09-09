ece56b0de73bf6382ed7388a8030e46365f9b470
DLMulMix <90311581+DLMulMix@users.noreply.github.com>
2021-09-09 09:48:03 +0800

    Update data-generate.sh

--- data-generate.sh
+++ data-generate.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-python3 generate.py /home/gb/yejunjie/mmt-coder/data-bin/en-de/test2016 \
-				--path /home/gb/yejunjie/mmt-coder/results/pre_mixup/mmtimg/model.pt \
+python3 generate.py data-bin/en-de/test2016 \
+				--path results/pre_mixup/mmtimg/model.pt \
 				--source-lang en --target-lang de \
 				--beam 5 \
 				--num-workers 12 \
 				--batch-size 128 \
-				--results-path /home/gb/yejunjie/mmt-coder/results/pre_mixup/mmtimg/results2016 \
+				--results-path results/pre_mixup/mmtimg/results2016 \
 				--remove-bpe \
 
