e89962ca4ac3d08b377fdc4aaa486884de8fd214
DLMulMix <90311581+DLMulMix@users.noreply.github.com>
2021-09-09 09:40:37 +0800

    Update data-train.sh

--- data-train.sh
+++ data-train.sh
@@ -6,7 +6,7 @@ python train.py data-bin/en-de \
 		--clip-norm 0 \
 		--optimizer adam \
 		--reset-optimizer \
-		--lr 0.0011 \
+		--lr 0.001 \
 		--source-lang en \
 		--target-lang de \
 		--max-tokens 1536 \
@@ -17,16 +17,16 @@ python train.py data-bin/en-de \
 		--criterion label_smoothed_cross_entropy \
 		--label-smoothing 0.2 \
 		--lr-scheduler inverse_sqrt \
-		--max-update 47000 \
+		--max-update 4700 \
 		--warmup-updates 4000 \
 		--warmup-init-lr 1e-07 \
 		--update-freq 4 \
 		--adam-betas 0.9,0.98 \
-		--keep-last-epochs 20 \
+		--keep-last-epochs 11 \
 		--dropout 0.3 \
 		--tensorboard-logdir results/en-de-bpe/bl_log1 \
 		--log-format simple \
-		--save-dir results/pre_mixup/mmtimg_0.2 \
+		--save-dir results/en-de/mmtimg \
 		--eval-bleu \
 		--patience 10 \
 		--fp16 \
