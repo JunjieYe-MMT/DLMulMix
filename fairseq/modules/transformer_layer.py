5c347c3925e419117486031651637787c70521c6
DLMulMix <90311581+DLMulMix@users.noreply.github.com>
2022-01-16 10:45:33 +0800

    update

--- fairseq/modules/transformer_layer.py
+++ fairseq/modules/transformer_layer.py
@@ -124,7 +124,7 @@ class TransformerEncoderLayer(nn.Module):
     def multimodel_mix(self, x, img, batch_len):
         #####  stra visual to textual  #########
         pseudo_features = img[torch.LongTensor(np.random.randint(0,img.size(0),batch_len))]
-        alpha = torch.tensor([random.betavariate(0.4,0.4)for _ in range(x.size(1))]).unsqueeze(0).unsqueeze(-1).type_as(x)
+        alpha = torch.tensor([random.betavariate(1,1)for _ in range(x.size(1))]).unsqueeze(0).unsqueeze(-1).type_as(x)
         mixed_x = alpha * x[:batch_len] + (1-alpha) * pseudo_features
         x = torch.cat([x[:batch_len], img, mixed_x], dim=0)
 
